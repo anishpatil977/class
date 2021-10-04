3713d4057fb0a06cc10233c669752303bb4157ab
anishpatil977 <75301919+anishpatil977@users.noreply.github.com>
2021-10-04 16:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+Webcam.set({
+
+width: 310,
+height: 300,
+image_format:'png',
+png_quality: 90,
+constraints:{
+
+facingMode:"environment"
+
+}
+});
+document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+
+Webcam.snap(function(data_uri) {
+ document.getElementById("result").innerHTML='<img id="captured image" src="'+data_uri+'"/>';
+
+});
+
+
+}
+
+console.log('ml5 version',ml5.version); 
+classifier=ml5.imageClassifier('Mobile Net', modelLoaded);
+
+function modelLoaded(){
+
+console.log('modelLoaded');
+
+}
+
+function check(){
+
+img=document.getElementById('captured_image');
+classifier.classify(img,gotresult);
+
+}
+
+function gotresult(error,results){
+
+if(error){
+    console.error(error);
+    
+}
+else{
+
+console.log(results);
+document.getElementById("object_name").innerHTML=results[0].label;
+}
+}
